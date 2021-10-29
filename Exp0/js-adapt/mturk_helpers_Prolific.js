32a18d7dd05856d0090ce65c63b88a6a10ff3c62
Xin Xie <isabelx.xie@gmail.com>
2021-10-28 23:50:19 -0700

    changed submission of data

--- Exp0/js-adapt/mturk_helpers_Prolific.js
+++ Exp0/js-adapt/mturk_helpers_Prolific.js
@@ -178,7 +178,8 @@ var mturk_end_surveys_and_submit = function() {
 //                      var header = "partOfExp, trialNumber, soundFile, word, keyCode, response, rtStart, rtEnd, rt <br>"; 
   //                    var myAns = $("#trainingResp").val().replace(';','<br>');
     //                  $('#live-answers').html(header + myAns);
-                      $("#mturk_form").submit();
+                     // $("#mturk_form").submit();
+                      $("#mturk_form").proliferate.submit();
                  });
             }
         }
